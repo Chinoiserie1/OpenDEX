9d63d5c706da9bb7bf19b52a514bbcc12ba869bb
chixx <lucottejeremie@gmail.com>
2024-04-20 13:47:48 +0200

    uppgrade OpenDexPair: addLiquidity

--- src/OpenDEXPair.sol
+++ src/OpenDEXPair.sol
@@ -1,9 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract OpenDexPair {
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+uint256 constant MINIMUM_LIQUIDITY = 10**3;
+
+error Reantrant();
+
+/**
+ * @notice Simple Decentralized Exchange by chixx.eth
+ */
+contract OpenDexPair is ERC20 {
+  address public factory;
   address public token0;
   address public token1;
 
+  uint256 private reserve0;
+  uint256 private reserve1;
+  uint256 private kVariable;
 
+  uint256 private reantrant = 1;
+
+  modifier reantrancyGuard {
+    if (reantrant == 0) revert Reantrant();
+    reantrant = 0;
+    _;
+    reantrant = 1;
+  }
+
+  constructor(address _factory, address _token0, address _token1) ERC20("LPToken", "LP") {
+    factory = _factory;
+    token0 = _token0;
+    token1 = _token1;
+  }
+
+  function addLiquidity(uint256 amount0, uint256 amount1) external reantrancyGuard returns(uint256 liquidity) {
+    uint256 totalSupply = totalSupply();
+    uint256 balance0 = IERC20(token0).balanceOf(address(this));
+    uint256 balance1 = IERC20(token1).balanceOf(address(this));
+    if (kVariable == 0) {
+      liquidity = amount0 * amount1 - MINIMUM_LIQUIDITY;
+      _mint(address(0), MINIMUM_LIQUIDITY);
+    } else {
+      uint256 liquidityX = amount0 * totalSupply / balance0;
+      uint256 liquidityY = amount1 * totalSupply / balance1;
+      liquidity = (liquidityX + liquidityY) / 2;
+    }
+
+    IERC20(token0).transferFrom(msg.sender, address(this), amount0);
+    IERC20(token1).transferFrom(msg.sender, address(this), amount1);
+
+    _mint(msg.sender, liquidity);
+
+    _syncReserve(balance0 + amount0, balance1 + amount1);
+  }
+
+  function _syncReserve(uint256 balance0, uint256 balance1) internal {
+    reserve0 = balance0;
+    reserve1 = balance1;
+  }
 }
